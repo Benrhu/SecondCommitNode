04ddbbcf158b418bae34276ebb5ad16673092f0a
benrhu <benrhu@gmail.com>
2022-04-26 22:02:15 +0200

    // csvTo_Array fixed

--- src/utils/csvToArray.ts
+++ src/utils/csvToArray.ts
@@ -1,14 +1,32 @@
 const csv = require('csv-parse')
 const fs = require('fs')
-const users = new Array
-const sinDuplicates = []
 const file = './users.csv'
 
 const reader = fs.createReadStream(file)
+const emails: string[] = []
 
-reader.on('data', function(chunk) {
-    console.log(chunk)
+reader.on('data', (data) => {
+  // Verificar emails duplicados.
+  data.forEach(row => {
+    const email: string | undefined = row[0]
+    if(!emailExists(email)){
+      emails.push(email)
+    }
+  });
+})
+
+reader.on('end', () => {
+    console.log(emails);
   })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  })
+
+const emailExists = (newEmail: string) => {
+  let exists: boolean = false
+
+  emails.forEach(email => {
+    if(email == newEmail){
+      exists = true
+    }
+  });
+
+  return exists
+}
